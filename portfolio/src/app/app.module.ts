8bf4dfa0f44eff7a9236fcea85745ec6fc01d28e
armedstar <armedstar@gmail.com>
2019-01-15 16:35:57 -0600

    built front-end for navigation

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderNavigationComponent } from './header-navigation/header-navigation.component';
+import { HeaderMainComponent } from './header-main/header-main.component';
+import { FooterComponent } from './footer/footer.component';
+import { ResultsComponent } from './results/results.component';
+import { BioComponent } from './bio/bio.component';
+import { SpeakingComponent } from './speaking/speaking.component';
+import { WorkshopsComponent } from './workshops/workshops.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderNavigationComponent,
+    HeaderMainComponent,
+    FooterComponent,
+    ResultsComponent,
+    BioComponent,
+    SpeakingComponent,
+    WorkshopsComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
