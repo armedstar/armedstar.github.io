54c2dda371220eaa403143f8083742f47044c29a
Amanda Mallardo <armedstar@gmail.com>
2024-06-03 12:57:06 -0500

    push to pages

---
+++ src/components/NavBar.js
@@ -0,0 +1,12 @@
+import {Navbar, Container, Nav} from "react-bootstrap";
+import logo from "../assets/logo.svg";
+import React, {useContext} from "react";
+
+export const NavBar = () => {
+
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+        <Navbar.Brand className="header"><span class="logo"><a href="#" onClick={() => window.location.reload()}><img src={logo} alt="Amanda Mallardo - Sr. Product Designer"></img></a></span><p className="name">Amanda Mallardo</p> <p>Sr. Product Designer</p></Navbar.Brand>
+    </Navbar>
+  );
+}
---
+++ src/pages/Rovi.js
@@ -0,0 +1,5 @@
+export const Rovi = () => {
+    return(
+        <p>Rovi Content</p>
+    )
+}
