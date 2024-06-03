6f346d911237198350b2dfa3e4106f54931b1487
Amanda Mallardo <armedstar@gmail.com>
2024-06-03 16:32:57 -0500

    Update Feature text
    Update Feature text

--- src/pages/HomeContent.js
+++ src/pages/HomeContent.js
@@ -24,7 +24,7 @@ export const HomeContent = () => {
                 <span><img src = {hereImg} alt="HERE Technologies"></img></span>
                 <div>
                 <h1>{'Helping a location data services company find their way'}</h1>
-                <p>HERE Technologies product line went from spin-offs and one-offs with a dispersed, disconnected set of product solutions to focusing on a comprehensive and connected platformed solution with products and solutions that serve broad market areas. </p>
+                <p>HERE Technologies product line went from one-offs with a dispersed, disconnected set of product solutions to a focused, comprehensive, connected platformed suite of solutions that serve broad market areas. </p>
                 <button onClick={() => setActive("ThirdView")}>Go to highlight</button>
                 </div>
                 </Col>
@@ -32,7 +32,7 @@ export const HomeContent = () => {
                 <span><img src = {roviImg} alt="TiVo"></img></span>
                 <div>
                 <h1>{'Guiding entertainment data into a new era of television'}</h1>
-                <p>In the advent of customizable and discoverable entertainment data, the Rovi R&D team paved the way for many of the personalized solutions you see today. Our work has created products and patents that are serving users to this day.</p>
+                <p>In the advent of customizable and discoverable entertainment content, the Rovi R&D team paved the way for many of the modern automated personalized solutions. Our work has created products and patents that are serving users to this day.</p>
                 <button onClick={() => setActive("View")}>Go to highlight</button>
                 </div>
                 </Col>
